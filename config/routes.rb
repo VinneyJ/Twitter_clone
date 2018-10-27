864261fb48cc04e65381b1a3322fd1dadf21ce3b
--v <vincentjayden49@gmail.com>
2018-10-27 16:01:35 +0300

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
+  get 'users/edit'
+
+  #get 'static pages/about'
+  resources :users
+  root "static_pages#home"
+
+  get "static_pages/contact"
+
+  get "static_pages/help"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
